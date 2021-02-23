dc8e8f542e6d0dd2c60ca312a4696be5de541e93
bayuapr <bayuapriansyah7@gmail.com>
2021-02-23 19:31:14 +0700

    channelGo

--- golang-goroutines/channel_test.go
+++ golang-goroutines/channel_test.go
@@ -1,9 +1,60 @@
 package golang_goroutines
 
 import (
+	"fmt"
 	"testing"
+	"time"
 )
 
 func TestCreateChannel(t *testing.T) {
 	channel := make(chan string)
+	defer close(channel)
+
+	go func() {
+		time.Sleep(2 * time.Second)
+		channel <- "Bayu Apriansyah"
+		fmt.Println("kirim data channel")
+	}()
+
+	data := <-channel
+	fmt.Println(data)
+
+	time.Sleep(5 * time.Second)
+}
+
+func GiveMeResponse(channel chan string) {
+	time.Sleep(2 * time.Second)
+	channel <- "BayuBy"
+}
+
+func TestChannelAsParameter(t *testing.T) {
+	channel := make(chan string)
+	defer close(channel)
+
+	go GiveMeResponse(channel)
+
+	data := <-channel
+	fmt.Println(data)
+
+	time.Sleep(5 * time.Second)
+}
+
+func OnlyIn(channel chan<- string) {
+	time.Sleep(2 * time.Second)
+	channel <- "Bayu Apriansyah"
+}
+
+func OnlyOut(channel <-chan string) {
+	data := <-channel
+	fmt.Println(data)
+}
+
+func TestInOutChannel(t *testing.T) {
+	channel := make(chan string)
+
+	go OnlyIn(channel)
+	go OnlyOut(channel)
+
+	time.Sleep(3 * time.Second)
+	close(channel)
 }
